86a1f3bf773cec4e66109fe85d7a519eea8f8ca0
zhehaowang <wangzhehao410305@gmail.com>
2015-11-06 16:18:12 -0800

    hw4: add initial plain kenken implementation

--- hw4/hw4.pl
+++ hw4/hw4.pl
@@ -1,13 +1,32 @@
+/*************************************************************************
+ * KenKen (fd) - kenken/3
+ *   @param N Size
+ *   @param C Set of constraint
+ *   @param T The result matrix
+ *
+ * This solution is rather fast: 
+ *   Finding all solutions to Example kenken 6 took 7ms, while
+ *   Example kenken 4 took 40ms
+ *************************************************************************/
 /**
- * Check if a certain element is in the given list
- * @param list
- * @param element
- * @param true | false
+ * Applying fd_domain to every element list of the given list (instead of
+ * every element of each element list), and applying fd_labeling as the last 
+ * seems to improve efficiency dramatically: commit d0ca39 could solve 4 * 4
+ * , but could not solve 6 * 6 in 5 minutes; but this version could do it in ms
+ *
+ * Note: probably should use transpose instead of column_all_distinct, does
+ * not seem to hurt efficiency much, so left here for now.
  */
-has_element([], _, false).
-has_element([H|_], H, true).
-has_element([H|T], X, Y) :- H \= X, has_element(T, X, Y).
+kenken(N, C, T) :- 
+  length(T, N), length_match(T, N), 
+  maplist(in_domain(N), T), 
+  meet_constraints(T, C), 
+  row_all_distinct(T), column_all_distinct(T, N), 
+  maplist(fd_labeling, T).
 
+/*************************************************************************
+ * Helper functions, could be replaced with built-in predicates
+ *************************************************************************/
 /**
  * Append a list to a given list
  * @param list1
@@ -16,18 +35,6 @@ has_element([H|T], X, Y) :- H \= X, has_element(T, X, Y).
  */
 append_list([], X, X).
 append_list([H|T], X, [H|TX]) :- append_list(T, X, TX).
-
-/**
- * Check if numbers in a list are unique
- * @param test_list
- * @param helper_list, should use []
- * @param true | false
- * TODO: using my is_distinct results in stack overflow, when doing "maplist(domain_1_2, [H|T]), is_distinct([H|T], [], true), maplist(fd_labeling, [H|T])",
- *   Seems that this does not terminate correctly
- */
-is_distinct([], _, true).
-is_distinct([H|T], Y, false) :- has_element(Y, H, true).
-is_distinct([H|T], Y, X) :- has_element(Y, H, false), append_list(Y, [H], YH), is_distinct(T, YH, X).
 
 /**
  * Get the i-th element from the list
@@ -41,31 +48,17 @@ get_n([H|T], I, X) :- I \= 0, N is I - 1, get_n(T, N, X).
 
 /**
  * Extract a column from a given matrix
- * Note: This logic is kind of weird: 
- *  we get the i-th element E of the current row H, try to get the i-th 
- *  element of the next row with some list Y, and make sure Y + E gives 
- *  our given extracted array X; since we know E is an array with one or 
- *  zero element, we can append list Y to it
  * @param list of lists
  * @param column number
  * @param resulting list
  */
 extract_column([], I, []).
-extract_column([H|T], I, X) :- get_n(H, I, E), extract_column(T, I, Y), append_list([E], Y, X).
+extract_column([H|T], I, X) :- 
+  get_n(H, I, E), 
+  extract_column(T, I, Y), 
+  append_list([E], Y, X).
 
-
-in_domain(X, N) :- fd_domain(X, 1, N).
-
-list_in_domain([], _).
-list_in_domain([H|T], N) :- in_domain(H, N), list_in_domain(T, N).
-
-all_in_domain([], _).
-all_in_domain([H|T], N) :- list_in_domain(H, N), all_in_domain(T, N).
-
-row_all_distinct([]).
-row_all_distinct([H|T]) :- fd_all_different(H), row_all_distinct(T).
-
-/*
+/**
  * This function really only checks that all lists in a list has length N.
  * To enforce that length match gives n * n, instead of 1 * n, we check length(X, N) as well as length_match(X, N) in sudoku rhs
  *
@@ -74,42 +67,126 @@ row_all_distinct([H|T]) :- fd_all_different(H), row_all_distinct(T).
 length_match([H|T], N) :- length(H, N), length_match(T, N).
 length_match([H], N) :- length(H, N).
 
+in_domain(N, L) :- fd_domain(L, 1, N).
+
 /**
- * Differentiate this with column_all_distinct(_, 0). ?
+ * Get the element at i-th row and j-th column
+ */
+get_i_j(X, I, J, Y) :- 
+  J1 is J - 1, I1 is I - 1, 
+  get_n(X, I1, X1), get_n(X1, J1, Y).
+
+/**
+ * Differentiate this with column_all_distinct(_, 0). 
  */
 column_all_distinct(_, 0) :- !.
-column_all_distinct(X, N) :- N1 is N - 1, extract_column(X, N1, T), fd_all_different(T), column_all_distinct(X, N1).
+column_all_distinct(X, N) :- 
+  N1 is N - 1, 
+  extract_column(X, N1, T), 
+  fd_all_different(T), 
+  column_all_distinct(X, N1).
 
-get_i_j(X, I, J, Y) :- J1 is J - 1, I1 is I - 1, get_n(X, I1, X1), get_n(X1, J1, Y).
+row_all_distinct([]).
+row_all_distinct([H|T]) :- 
+  fd_all_different(H), 
+  row_all_distinct(T).
 
+/*************************************************************************
+ * Numeric constraint functions
+ *************************************************************************/
 /**
- * The sequence of "sum_matrix(X, T, Y1)" and "Y is Y1 + X2" does matter, putting the evaluation first would cause instantiation error
+ * The sequence of "sum_matrix(X, T, Y1)" and "Y is Y1 + X2" does matter,
+ * putting the evaluation first would cause instantiation error; 
+ * Differentiate '#=' and 'is': Using '#=' also solves the problem above
  */
 sum_matrix(_, [], 0).
-sum_matrix(X, [I-J|T], Y) :- get_i_j(X, I, J, E), sum_matrix(X, T, Y1), Y is Y1 + E.
-multiply_matrix(_, [], 1).
-multiply_matrix(X, [I-J|T], Y) :- get_i_j(X, I, J, E), multiply_matrix(X, T, Y1), Y is Y1 * E.
+sum_matrix(X, [I-J|T], Y) :- 
+  get_i_j(X, I, J, E), 
+  sum_matrix(X, T, Y1), 
+  Y #= Y1 + E.
 
-minus_matrix(X, I1-J1, I2-J2, Y) :- get_i_j(X, I1, J1, X1), get_i_j(X, I2, J2, X2), Y is X1 - X2.
-divide_matrix(X, I1-J1, I2-J2, Y) :- get_i_j(X, I1, J1, X1), get_i_j(X, I2, J2, X2), Y is X1 / X2.
+multiply_matrix(_, [], 1).
+multiply_matrix(X, [I-J|T], Y) :- 
+  get_i_j(X, I, J, E), 
+  multiply_matrix(X, T, Y1), 
+  Y #= Y1 * E.
+
+minus_matrix(X, I1-J1, I2-J2, Y) :- 
+  get_i_j(X, I1, J1, X1), 
+  get_i_j(X, I2, J2, X2), 
+  Y #= X1 - X2.
+
+divide_matrix(X, I1-J1, I2-J2, Y) :- 
+  get_i_j(X, I1, J1, X1), 
+  get_i_j(X, I2, J2, X2), 
+  Y #= X1 / X2.
 
 meet(X, +(Y, [H|T])) :- sum_matrix(X, [H|T], Y).
 meet(X, *(Y, [H|T])) :- multiply_matrix(X, [H|T], Y).
-meet(X, -(Y, P1, P2)) :- minus_matrix(X, P1, P2, Y); minus_matrix(X, P2, P1, Y).
-meet(X, /(Y, P1, P2)) :- divide_matrix(X, P1, P2, Y); minus_matrix(X, P1, P2, Y).
+meet(X, -(Y, P1, P2)) :- 
+  minus_matrix(X, P1, P2, Y); 
+  minus_matrix(X, P2, P1, Y).
+meet(X, /(Y, P1, P2)) :- 
+  divide_matrix(X, P1, P2, Y); 
+  divide_matrix(X, P2, P1, Y).
 
 meet_constraints(X, []).
 meet_constraints(X, [H|T]) :- meet(X, H), meet_constraints(X, T).
 
 /**
- * Need to maplist first, then meet_constraints, otherwise it is trying to match some list values to meet constraints function
+ * KenKen (fd) with no constraints, for fun and testing purposes
  */
-kenken(N, C, T) :- length(T, N), length_match(T, N), all_in_domain(T, N), row_all_distinct(T), column_all_distinct(T, N), maplist(fd_labeling, T), meet_constraints(T, C).
+sudoku(N, X) :- 
+  length(X, N), length_match(X, N), 
+  maplist(set_domain(N), X), 
+  row_all_distinct(X), 
+  column_all_distinct(X, N), 
+  maplist(fd_labeling, X).
 
-sudoku(0, []).
-sudoku(N, X) :- length(X, N), length_match(X, N), all_in_domain(X, N), row_all_distinct(X), column_all_distinct(X, N), maplist(fd_labeling, X).
+/*************************************************************************
+ * KenKen (plain) - kenken/3
+ * KenKen (plain) does not use fd_domain, fd_all_different, and fd_labeling
+ *   The idea is to use permutations of int list instead of fd_domain
+ *   Interesting note though, perm in class can check if something is a perm 
+ *   of given list, but not list all the permutations, we use the built-in
+ *   permutation for now
+ *
+ * This permutation-based solution is really slow:
+ *   Finding all Solutions to Example 4 took 51s
+ *************************************************************************/
+plain_kenken(N, C, T) :- 
+  length(T, N), length_match(T, N), get_domain_list(N, R),
+  plain_domain(T, N),
+  meet_constraints(T, C), plain_column_all_distinct(T, N, R).
 
-kenken_testcase(
+/**
+ * Note: the difference between putting append_list(R, [N], R1) and append_list
+ * (R1, [N], R) here...why do we have to add length(R, N)?
+ */
+get_domain_list(0, _) :- !.
+get_domain_list(N, R) :-
+  length(R, N),
+  N1 is N - 1,
+  append_list(R1, [N], R),
+  get_domain_list(N1, R1).
+
+plain_domain([], _) :- !.
+plain_domain([H|T], N) :-
+  get_domain_list(N, P), 
+  permutation(P, H),
+  plain_domain(T, N).
+
+plain_column_all_distinct(_, 0, _) :- !.
+plain_column_all_distinct(X, N, R) :- 
+  N1 is N - 1, 
+  extract_column(X, N1, T), 
+  permutation(T, R),
+  plain_column_all_distinct(X, N1, R).
+
+/*************************************************************************
+ * KenKen built-in test cases
+ *************************************************************************/
+zhehao_kenken_testcase(
   4,
   [
    +(6, [1-1, 1-2, 2-1]),
@@ -121,7 +198,7 @@ kenken_testcase(
   ]
 ).
 
-kenken_testcase1(
+zhehao_kenken_testcase1(
   6,
   [
    +(11, [1-1, 2-1]),
